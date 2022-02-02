65cc13fdbef16d2172ebd84d3a0ec93214c34d44
r0p06np <rabindra.kumar.patra@walmart.com>
2022-02-02 11:07:42 +0530

    added swap sort repeat and missing element

--- src/main/java/dsa/tuf/day2/App.java
+++ src/main/java/dsa/tuf/day2/App.java
@@ -1,11 +1,45 @@
 package dsa.tuf.day2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
 
     }
 
+    // max value in array of size n is n+1
+    public List<Integer> repeatAndMissingNumberArray(int[] arr){
+        for (int i = 0; i < arr.length; ) {
+            int tempIndex = arr[i] - 1;
+            if (tempIndex >= arr.length) {
+                i++;
+                continue;
+            }
+            if (arr[i] != arr[tempIndex]) {
+                int t = arr[tempIndex];
+                arr[tempIndex] = arr[i];
+                arr[i] = t;
+            } else {
+                i++;
+            }
+        }
+
+        // duplicate value at ith position value
+        // missing value is ith_index + 1
+        System.out.println(Arrays.toString(arr));
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]-1 !=i){
+                list.add(arr[i]);
+                list.add(i+1);
+            }
+        }
+        System.out.println(list);
+        return list;
+    }
     // _ARRAYS
     public int missingElementInSortedArray(int[] arr) {
         int l = 0;
--- src/test/java/dsa/tuf/day2/AppTest.java
+++ src/test/java/dsa/tuf/day2/AppTest.java
@@ -4,6 +4,9 @@ package dsa.tuf.day2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AppTest {
 
     @Test
@@ -11,7 +14,7 @@ public class AppTest {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18};
         App app = new App();
         int actualValue = app.missingElementInSortedArray(arr);
-        Assertions.assertEquals(16,actualValue);
+        Assertions.assertEquals(16, actualValue);
     }
 
     @Test
@@ -19,6 +22,24 @@ public class AppTest {
         int[] arr = {1, 2, 3, 4, 6, 7, 8, 9};
         App app = new App();
         int actualValue = app.missingElementInSortedArray(arr);
-        Assertions.assertEquals(5,actualValue);
+        Assertions.assertEquals(5, actualValue);
+    }
+
+    @Test
+    void repeatAndMissingNumberArray_1() {
+        App app = new App();
+        int[] arr = {2, 3, 1, 4, 5, 1, 5};
+        List<Integer> actualList = app.repeatAndMissingNumberArray(arr);
+        List<Integer> expectedList = Arrays.asList(1,6,5,7);
+        Assertions.assertEquals(expectedList, actualList);
+    }
+
+    @Test
+    void repeatAndMissingNumberArray_2() {
+        App app = new App();
+        int[] arr = {1, 2, 3, 4, 5, 1, 5};
+        List<Integer> actualList = app.repeatAndMissingNumberArray(arr);
+        List<Integer> expectedList = Arrays.asList(1,6,5,7);
+        Assertions.assertEquals(expectedList, actualList);
     }
 }
