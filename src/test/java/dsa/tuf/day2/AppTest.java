b8dfc29ccaddafe98733e8607cd76f3055d5885d
r0p06np <rabindra.kumar.patra@walmart.com>
2022-02-06 21:45:08 +0530

    merge sorted array without extra space

--- src/test/java/dsa/tuf/day2/AppTest.java
+++ src/test/java/dsa/tuf/day2/AppTest.java
@@ -30,7 +30,7 @@ public class AppTest {
         App app = new App();
         int[] arr = {2, 3, 1, 4, 5, 1, 5};
         List<Integer> actualList = app.repeatAndMissingNumberArray(arr);
-        List<Integer> expectedList = Arrays.asList(1,6,5,7);
+        List<Integer> expectedList = Arrays.asList(1, 6, 5, 7);
         Assertions.assertEquals(expectedList, actualList);
     }
 
@@ -39,7 +39,67 @@ public class AppTest {
         App app = new App();
         int[] arr = {1, 2, 3, 4, 5, 1, 5};
         List<Integer> actualList = app.repeatAndMissingNumberArray(arr);
-        List<Integer> expectedList = Arrays.asList(1,6,5,7);
+        List<Integer> expectedList = Arrays.asList(1, 6, 5, 7);
         Assertions.assertEquals(expectedList, actualList);
+    }
+
+    @Test
+    void mergeSortedArraysWithoutExtraSpace() {
+
+
+        int arr1[] = {1, 2, 4, 6, 8, 9};
+        int arr2[] = {3, 5, 7};
+        App app = new App();
+        app.mergeSortedArray(arr1, arr2);
+    }
+
+    @Test
+    void mergeSortedArrayWithoutExtraSpace_1() {
+        int arr1[] = {1, 8};
+        int arr2[] = {4, 5};
+        App app = new App();
+        app.mergeSortedArrayWithoutExtraSpace(arr1, arr2);
+        Assertions.assertArrayEquals(new int[]{1, 4}, arr1);
+        Assertions.assertArrayEquals(new int[]{5, 8}, arr2);
+    }
+
+    @Test
+    void mergeSortedArrayWithoutExtraSpace_2() {
+        int arr1[] = {1, 2};
+        int arr2[] = {3, 4};
+        App app = new App();
+        app.mergeSortedArrayWithoutExtraSpace(arr1, arr2);
+        Assertions.assertArrayEquals(new int[]{1, 2}, arr1);
+        Assertions.assertArrayEquals(new int[]{3, 4}, arr2);
+    }
+
+    @Test
+    void mergeSortedArrayWithoutExtraSpace_3() {
+        int arr1[] = {1, 2};
+        int arr2[] = {3, 4, 5};
+        App app = new App();
+        app.mergeSortedArrayWithoutExtraSpace(arr1, arr2);
+        Assertions.assertArrayEquals(new int[]{1, 2}, arr1);
+        Assertions.assertArrayEquals(new int[]{3, 4, 5}, arr2);
+    }
+
+    @Test
+    void mergeSortedArrayWithoutExtraSpace_4() {
+        int arr1[] = {1, 6};
+        int arr2[] = {3, 4, 5};
+        App app = new App();
+        app.mergeSortedArrayWithoutExtraSpace(arr1, arr2);
+        Assertions.assertArrayEquals(new int[]{1, 3}, arr1);
+        Assertions.assertArrayEquals(new int[]{4, 5, 6}, arr2);
+    }
+
+    @Test
+    void mergeSortedArrayWithoutExtraSpace_5() {
+        int arr1[] = {1, 6, 7, 8};
+        int arr2[] = {3, 4, 5};
+        App app = new App();
+        app.mergeSortedArrayWithoutExtraSpace(arr1, arr2);
+        Assertions.assertArrayEquals(new int[]{1, 3, 4, 5}, arr1);
+        Assertions.assertArrayEquals(new int[]{6, 7, 8}, arr2);
     }
 }
